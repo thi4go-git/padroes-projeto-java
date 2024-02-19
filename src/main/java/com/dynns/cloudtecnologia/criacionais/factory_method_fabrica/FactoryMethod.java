1004cc9fd634889c4c7e1f271c3a6e46ad7739c1
thi4go-git <thi4go19@gmail.com>
2024-02-19 16:26:56 -0300

    Commit inicial

---
+++ src/main/java/com/dynns/cloudtecnologia/criacionais/factory_method_fabrica/FactoryMethod.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.criacionais.factory_method_fabrica;
+
+/*
+Problemas: 
+-Como posso escrever um código onde as classes instanciadas possam variar dentro
+de uma mesma interface?
+-Como deixar o meu código desacoplado das classes concretas?
+
+*/
+
+public class FactoryMethod {
+
+}
